963795ae90e1385f1141176b727ee0c93a7e0d78
dev ian <markianebuen@gmail.com>
2024-04-14 16:03:42 +0800

    Creating backpressed in mainactivity

--- app/src/main/java/Activities/MainActivity.java
+++ app/src/main/java/Activities/MainActivity.java
@@ -1,8 +1,10 @@
 package Activities;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -21,8 +23,52 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
-
         });
+
+
+
+
+
+        //Methods
+        onbackedpressed();
+
+
+    }
+    private void onbackedpressed(){
+        // Create a callback for back button press
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                // Handle back button press
+
+                // Create an AlertDialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Are you sure you want to close the application?");
+                builder.setCancelable(true);
+
+                // postive button obvious naman diba hahahaha
+                builder.setPositiveButton(
+                        "Yes",
+                        (dialog, id) -> {
+                            finish();
+
+                        });
+
+                // Add negative button
+                builder.setNegativeButton(
+                        "No",
+                        (dialog, id) -> {
+                            // Dismiss the dialog when "No" is clicked
+                            dialog.cancel();
+                        });
+
+                // Create and show the AlertDialog
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        };
+
+        // Add callback to the OnBackPressedDispatcher
+        getOnBackPressedDispatcher().addCallback(this, callback);
     }
 }
